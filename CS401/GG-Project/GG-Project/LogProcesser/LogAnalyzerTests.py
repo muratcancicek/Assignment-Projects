ccf28fd3f8dfa51b087663e94f6355345887d151
miek <miek@L-IST-14500667>
2017-02-16 19:03:46 +0300

    First queries for search journeys has been implemented.

--- CS401/GG-Project/GG-Project/LogProcesser/LogAnalyzerTests.py
+++ CS401/GG-Project/GG-Project/LogProcesser/LogAnalyzerTests.py
@@ -1,4 +1,5 @@
 from LogProcesser.LogAnalyzer import *
+import datetime
 
 def basicTests(): # Running successfully
     logs = readLogs(TEST_LOGS)
@@ -42,10 +43,78 @@ def mapReduceTests(logs = None): # Running successfully
     
 def newTest(logs = None): # Under development 
     logs = getLogs(logs)
-    modules = getLogsColumnAsList('module', logs)
-    print list(set(modules))
+    #modules = getLogsColumnAsList('module', logs)
+    #print list(set(modules))
     paymentLogs, indices = getLogsWhereValue('payment', 'module', logs = logs)
-    print len(paymentLogs), paymentLogs[0]
-    selectedLogs, indices = getLogsWhereValue(['payment', 'newseasion', 'search', 'chart'], 'module', logs = logs)
-    print len(selectedLogs), selectedLogs[0]
-    pass
+    searchLogs, indices = getLogsWhereValue('search', 'module', logs = logs)
+    itemLogs, indices = getLogsWhereValue('item', 'module', logs = logs)
+    cartLogs, indices = getLogsWhereValue('cart', 'module', logs = logs)
+    print len(paymentLogs), len(searchLogs), len(itemLogs), len(cartLogs)
+    for log in searchLogs:
+        if 'ids' in log.keys():
+            log['ids'] = log['ids'].split('%2C')
+        else:
+          searchLogs.remove(log)
+   
+    paymentIds = [log['id'] for log in paymentLogs]
+    
+    #for log in cartLogs :
+    #    print log
+   
+    cookies = [log['_c'] for log in cartLogs]
+    cookiesSet = list(set(cookies))
+    print len(cookies) , len(cookiesSet) 
+
+    for cookie in cookiesSet:
+        cookies.remove(cookie)
+
+    #for cookie in cookies:
+    #    print(cookie)
+
+    print  len(paymentIds)
+    cartIds = [log['id'] for log in cartLogs]
+    
+    print len(cartIds)
+    commonIds = [id for id in paymentIds if id in cartIds]
+    print len( commonIds)
+
+    paymentCookies = []
+
+    for paymentLog in paymentLogs:
+        paymentCookies.append({'id':paymentLog['id'], '_c': paymentLog['_c']}) 
+        #print paymentLog['id'], paymentLog['_c']
+    myLogs = []
+    
+    for paymentLog in paymentCookies:
+        for log in searchLogs+itemLogs+cartLogs+paymentLogs:
+            if log["module"] =="search": 
+                if '_c' in log.keys():
+                    if 'ids' in log.keys() and paymentLog['id'] in log['ids'] and paymentLog["_c"] == log["_c"]:
+                        myLogs.append(log) 
+                else:
+                    if 'ids' in log.keys() and paymentLog['id'] in log['ids']:
+                        myLogs.append(log) 
+            else:
+                if '_c' in log.keys():
+                    if 'id' in log.keys() and paymentLog['id'] == log['id'] and paymentLog["_c"] == log["_c"]:
+                        myLogs.append(log) 
+                else:
+                    if 'id' in log.keys() and paymentLog['id'] == log['id']:
+                        myLogs.append(log)
+       
+
+
+    
+    for log in myLogs :
+        if  '_c' in log.keys() and log["_c"]=="9b3c7d6da4fbae9a258c36f8bb9bb40e8d29d963547a539ddd2e998336daf234":
+            log["time"] = datetime.datetime.fromtimestamp(int(log["timestamp"])/ 1e3) 
+            print log["time"]
+            print log
+
+    
+
+
+    #for log 
+    #selectedLogs, indices = getLogsWhereValue(['payment', 'newseasion', 'search', 'chart'], 'module', logs = logs)
+    #print len(selectedLogs), selectedLogs[0]
+    #pass
