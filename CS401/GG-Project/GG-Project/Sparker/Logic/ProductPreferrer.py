de18ea812cd5c9fe7817c90685b2e2408ae5e91d
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-04-02 21:48:17 +0300

    More sophisticated logic has been implemented.

--- CS401/GG-Project/GG-Project/Sparker/Logic/ProductPreferrer.py
+++ CS401/GG-Project/GG-Project/Sparker/Logic/ProductPreferrer.py
@@ -6,39 +6,32 @@ from MainSrc.PythonVersionHandler import *
 from Sparker.PySparkImports import *
 import Sparker.SparkLogProcesser.SparkLogAnalyzer as SLA
 
-def getListedIdsFromJourney(journey):
-    searches = journey.filter(SLA.isSearchLog)
-    return getIdsFromSearches(searches)#sc_().parallelize().distinct()#unique()
-
-def getLoggedIds(journey, module):
-    interestingLogs = sortedLogs(journey.filter(lambda log: log['module'] == module))
-    return interestingLogs.map(lambda log: (log['id']))
-
 def printActionsByModule(logs, module):
     printActions(logs.filter(lambda log: log['module'] == module))
 
-def summaryIds(ids):
-    #print_(ids['listed'])
-    #print_(ids['paidCnt'].collect())
-    #print_(ids['cartCnt'].collect())
-    #print_(ids['clickedCnt'].collect())
-    print_('Listed counts =', len(ids['listed']), 'Paid counts =', ids['paidCnt'].count(), 
-           'Cart counts =', ids['cartCnt'].count(), 'Clicked counts =', ids['clickedCnt'].count())# ids['listed'].count(), 
-    
-def cleanCount(ids):
-    #ids['cartCnt'] = ids['cartCnt'].subtractByKey(ids['paidCnt'])
-    #ids['clickedCnt'] = ids['clickedCnt'].subtractByKey(ids['paidCnt']).subtractByKey(ids['cartCnt'])
-    #return ids 
-    s = ids['listed']
-    ids['paidCnt'] = ids['paidCnt'].filter(lambda l: l[0] in s)
-    ids['cartCnt'] = ids['cartCnt'].filter(lambda l: l[0] in s).subtractByKey(ids['paidCnt'])
-    ids['clickedCnt'] = ids['clickedCnt'].filter(lambda l: l[0] in s).subtractByKey(ids['paidCnt']).subtractByKey(ids['cartCnt'])
-    return ids 
+def getListedIdsFromJourney(journey):
+    searches = journey.filter(SLA.isSearchLog)
+    return getIdsFromSearches(searches)
+
+def getLoggedIds(journey, module):
+    interestingLogs = journey.filter(lambda log: log['module'] == module).sortBy(lambda log: log['timestemp'])
+    return interestingLogs.map(lambda log: (log['id']))
 
 def countIds(ids):
     for process in ['paid', 'cart', 'clicked']:
-        ids[process+'Cnt'] =  sc_().parallelize(ids[process].countByValue().items())
+        ids[process+'Cnt'] =  sc_().parallelize(ids[process].countByKey().items())
+    ids['listedCnt'] = sc_().parallelize(listed).countByKey().items()
     return ids
+
+def summaryIds(ids):
+    print_('Listed counts =', len(ids['listed']), 'Paid counts =', ids['paidCnt'].count(), 
+           'Cart counts =', ids['cartCnt'].count(), 'Clicked counts =', ids['clickedCnt'].count())
+    
+def cleanCount(ids):
+    ids['paidCnt'] = ids['paidCnt'].filter(lambda l: l[0] in ids['listed'])
+    ids['cartCnt'] = ids['cartCnt'].filter(lambda l: l[0] in ids['listed']).subtractByKey(ids['paidCnt'])
+    ids['clickedCnt'] = ids['clickedCnt'].filter(lambda l: l[0] in ids['listed']).subtractByKey(ids['paidCnt']).subtractByKey(ids['cartCnt'])
+    return ids 
 
 def modulizeIds(journey):
     productLogs = journey.filter(isProductLog)
@@ -47,7 +40,6 @@ def modulizeIds(journey):
     'paid': getLoggedIds(productLogs, 'payment'),
     'cart': getLoggedIds(productLogs, 'cart'),
     'clicked': getLoggedIds(productLogs, 'item') }
-    #summaryIds(ids)
     ids = countIds(ids)
     summaryIds(ids)
     ids = cleanCount(ids)
@@ -65,6 +57,8 @@ def getLabeledPairsWithModulizedIds(journey):
     paidCnt = ids['paidCnt'].collect()
     cartCnt = ids['cartCnt'].collect()
     clickedCnt = ids['clickedCnt'].collect()
+    clickedIds = [id for id, v in clickedCnt]
+    levels = [paidCnt, cartCnt, clickedCnt, ids['listedCnt']]
     labeledPairs = {}
 
     def addDominantPair(id1, id2): 
@@ -75,15 +69,33 @@ def getLabeledPairsWithModulizedIds(journey):
     def labelByValues(v1, v2):
         return positive if v1 > v2 else negative
 
-    def addPairByValue(id1, id2, v1, v2): 
-        #if v1 == v2: return 
+    def labelBySubValues(id1, id2, level):
+        if level == len(levels):
+            v1 = clickedIds.index(id1)
+            v2 = clickedIds.index(id2)
+            addPairByValue(id1, id2, v1, v2, level)
+        cnts = levels[level]  
+        v1, v2 = None, None
+        for id, v in cnts:
+            if id == id1: v1 = v
+            if id == id2: v2 = v
+        if v1 == None:
+            addPairByValue(id1, id2, -1, v2, level)
+        elif v2 == None:
+            addPairByValue(id1, id2, v1, -1, level)
+        else:
+            addPairByValue(id1, id2, v1, v2, level)
+
+    def addPairByValue(id1, id2, v1, v2, level): 
+        if v1 == v2: 
+            labelBySubValues(id1, id2, level + 1)
         if id1 != id2:
             labeledPairs[keyPairIds(id1, id2)] = labelByValues(v1, v2)
             labeledPairs[keyPairIds(id2, id1)] = labelByValues(v2, v1)
 
     for paidId, pCount in paidCnt:
         for paidId2, pCount2 in paidCnt:
-             addPairByValue(paidId, paidId2, pCount, pCount2)
+             addPairByValue(paidId, paidId2, pCount, pCount2, level = 0)
         for cartId, cartCount in cartCnt:
              addDominantPair(paidId, cartId)
         for clickedId, clickedCount in clickedCnt:
@@ -91,19 +103,14 @@ def getLabeledPairsWithModulizedIds(journey):
              
     for cartId, cartCount in cartCnt:
         for cartId2, cartCount2 in cartCnt:
-             addPairByValue(cartId, cartId2, cartCount, cartCount2)
+             addPairByValue(cartId, cartId2, cartCount, cartCount2, level = 1)
         for clickedId, clickedCount in clickedCnt:
              addDominantPair(cartId, clickedId)
              
     for clickedId, clickedCount in clickedCnt:
         for clickedId2, clickedCount2 in clickedCnt:
-             addPairByValue(clickedId, clickedId2, clickedCount, clickedCount2)
+             addPairByValue(clickedId, clickedId2, clickedCount, clickedCount2, level = 2)
     #labeledPairs.saveAsTextFile('hdfs://osldevptst01.host.gittigidiyor.net:8020/user/root/data/part0&1_iphone_6')
     ids['labeledPairs'] = sc_().parallelize([(key, v) for key, v in labeledPairs.items()])
     print_(ids['labeledPairs'].count(), ' labeled pairs has been generated by', nowStr())
     return ids
-
-def getInterestingIds(journey): 
-    labeledPairs = getLabeledPairsWithModulizedIds(journey)
-    #print_(labeledPairs)
-    return labeledPairs
