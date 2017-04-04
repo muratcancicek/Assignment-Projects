ad4a000091db25f5fee36aafe5e56a7e3cdce7e1
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-04-04 16:31:41 +0300

    .

--- CS401/GG-Project/GG-Project/MainSrc/SparkerMethods.py
+++ CS401/GG-Project/GG-Project/MainSrc/SparkerMethods.py
@@ -40,6 +40,6 @@ def runSparkLogOperatorTests(logs):
 
 def run(): 
     sc = SparkContext() 
-    sc.setLocalProperty('spark.executor.instances', 24) 
+    sc.setSystemProperty('spark.executor.instances', 24) 
     setSparkContext(sc)
     trainLocalDataTest()
