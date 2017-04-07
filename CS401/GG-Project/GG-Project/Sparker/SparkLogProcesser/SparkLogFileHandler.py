6810f82349e60e17c37c7b578bf7cb1f50fb0f7b
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-04-07 16:02:59 +0300

    parse all day

--- CS401/GG-Project/GG-Project/Sparker/SparkLogProcesser/SparkLogFileHandler.py
+++ CS401/GG-Project/GG-Project/Sparker/SparkLogProcesser/SparkLogFileHandler.py
@@ -162,5 +162,5 @@ def saveRDDToHDFS(rdd, fileName):
     print_(fileName, 'has been saved successfully by', nowStr())
 
 def parseAllDay(allDayPath, outputPath):
-    logs = sc_().textFile(allLogsPath).sortBy(lambda log: log['timestamp'])
+    logs = getLogs(None, allDayPath).sortBy(lambda log: log['timestamp'])
     saveRDDToHDFS(logs, outputPath)
