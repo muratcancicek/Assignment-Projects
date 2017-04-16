87d198c993b94db6705f741e19057cd99cc0596e
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-04-17 02:29:23 +0300

    .

--- CS401/GG-Project/GG-Project/Sparker/Logic/LogicTests.py
+++ CS401/GG-Project/GG-Project/Sparker/Logic/LogicTests.py
@@ -68,9 +68,7 @@ def generateJourney(logs, keyword):
 def generateJourneys():
     logs = readParsedLogsFromHDFS(entireDayParsedLogsFolder1)
     keywords = ['nike air max', 'spor ayyakabı', 'tv unitesi', 'kot ceket', 'camasir makinesi', 'bosch', 'köpek maması']
-    #u = u'g'
-    #print_(type(u.lower()), type(u.encode('utf-8')))
-    for keyword in keywords:
+    for keyword in keywords[1:]:
         #print_('Day1_' + keyword.replace(' ', '_') + '_Data')
         generateJourney(logs, keyword)
 
