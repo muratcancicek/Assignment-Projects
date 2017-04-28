2afc1e601c71377120776a68c80dc3966572755f
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-04-28 21:10:12 +0300

    .

--- CS466/MNIST_Tensorflow/MNIST_Tensorflow/Main.py
+++ CS466/MNIST_Tensorflow/MNIST_Tensorflow/Main.py
@@ -16,20 +16,20 @@ def getXY_(n = 784, clusters = 10):
 def learn(mnist, iterations = 100, downsampling = False, dataset = ''):
     xSize = mnist.train.images.shape[1]
     x, y_ = getXY_(xSize)
-    if sys.argv[1] == 'network_1':
-        text = 'The First Network on ' + dataset
-        runCFirstCustomCNN(mnist, x, y_, xSize, cnn = firstCNN, iterations = iterations, downsampling = downsampling, text = text)
-    
     if sys.argv[1] == 'network_2':
         text = 'The Second Network on ' + dataset
         runCFirstCustomCNN(mnist, x, y_, xSize, cnn = secondCNN, iterations = iterations, downsampling = downsampling, text = text)
+    else:
+        text = 'The First Network on ' + dataset
+        runCFirstCustomCNN(mnist, x, y_, xSize, cnn = firstCNN, iterations = iterations, downsampling = downsampling, text = text)
+    
 
 def main(): 
     print_('You must pass exactly 3 arguments to this program, ' + \
         '\nExample usage: \npython muratcan_cicek_training.py network_1 28x28_dataset /path/to/dataset/folder')
     if len(sys.argv) != 4:
         print_('wrong argument count')
-        sys.exit() 
+        #sys.exit() 
 
     if sys.argv[2] == '28x28_dataset':
         original_mnist = loadOriginalMNIST()
@@ -40,6 +40,9 @@ def main():
     if sys.argv[2] == '14x14_augmented_dataset':
         augmented_mnist = generateAugmentedMNIST()
         learn(augmented_mnist, downsampling = True, dataset = '14x14_augmented_dataset')
+    else:
+        original_mnist = loadOriginalMNIST()
+        learn(original_mnist, dataset = '28x28_dataset')
 
 #cd Assignment-Projects/CS466/MNIST_Tensorflow/MNIST_Tensorflow
 #  python muratcan_cicek_training.py network_1 28x28_dataset D:\OneDrive\Projects\Assignment-Projects\CS466\MNIST_Tensorflow\MNIST_Tensorflow
