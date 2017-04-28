ce5a639f26ea1a5a18765f6d0f50517c6bdd2e72
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-04-28 21:44:22 +0300

    u

--- CS466/MNIST_Tensorflow/MNIST_Tensorflow/Main.py
+++ CS466/MNIST_Tensorflow/MNIST_Tensorflow/Main.py
@@ -8,6 +8,7 @@ from myMnistHandler import load_mnist
 from PythonVersionHandler import *
 import tensorflow as tf
 
+sys_argv = ['', 'network_1', '28x28_dataset', '/path/to/dataset/folder'] 
 def getXY_(n = 784, clusters = 10):
     x = tf.placeholder(tf.float32, [None, n])
     y_ = tf.placeholder(tf.float32, [None, clusters])
@@ -16,7 +17,7 @@ def getXY_(n = 784, clusters = 10):
 def learn(mnist, iterations = 100, downsampling = False, dataset = ''):
     xSize = mnist.train.images.shape[1]
     x, y_ = getXY_(xSize)
-    if sys.argv[1] == 'network_2':
+    if sys_argv[1] == 'network_2':
         text = 'The Second Network on ' + dataset
         runCFirstCustomCNN(mnist, x, y_, xSize, cnn = secondCNN, iterations = iterations, downsampling = downsampling, text = text)
     else:
@@ -26,18 +27,18 @@ def learn(mnist, iterations = 100, downsampling = False, dataset = ''):
 
 def main(): 
     print_('You must pass exactly 3 arguments to this program, ' + \
-        '\nExample usage: \npython muratcan_cicek_training.py network_1 28x28_dataset /path/to/dataset/folder')
-    #if len(sys.argv) != 4:
-    #    print_('wrong argument count')
-        #sys.exit() 
+        '\nExample usage: \npython muratcan_cicek_training.py network_1 28x28_dataset', '/path/to/dataset/folder')
+    if len(sys_argv) != 4:
+        print_('wrong argument count')
+        sys.exit() 
 
-    if sys.argv[2] == '28x28_dataset':
+    if sys_argv[2] == '28x28_dataset':
         original_mnist = loadOriginalMNIST()
         learn(original_mnist, dataset = '28x28_dataset')
-    if sys.argv[2] == '14x14_dataset':
+    if sys_argv[2] == '14x14_dataset':
         original_mnist = loadOriginalMNIST()
         learn(original_mnist, downsampling = True, dataset = '14x14_dataset')
-    if sys.argv[2] == '14x14_augmented_dataset':
+    if sys_argv[2] == '14x14_augmented_dataset':
         augmented_mnist = generateAugmentedMNIST()
         learn(augmented_mnist, downsampling = True, dataset = '14x14_augmented_dataset')
     else:
