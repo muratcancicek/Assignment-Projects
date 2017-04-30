b889ff1d70e7f4117e6644bc30e112e54834fbf1
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-05-01 00:20:14 +0300

    .

--- CS466/CIFAR10_Tensorflow/CIFAR10_Tensorflow/tfFLAGS.py
+++ CS466/CIFAR10_Tensorflow/CIFAR10_Tensorflow/tfFLAGS.py
@@ -26,16 +26,16 @@ NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
 FLAGS = tf.app.flags.FLAGS
+if COMPUTERNAME == 'MSI' or COMPUTERNAME == 'LM-IST-00UBFVH8':
+    # Basic model parameters.
+    tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
+    tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data', """Path to the CIFAR-10 data directory.""")
+    tf.app.flags.DEFINE_boolean('use_fp16', False, """Train the model using fp16.""")
 
-# Basic model parameters.
-tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data', """Path to the CIFAR-10 data directory.""")
-tf.app.flags.DEFINE_boolean('use_fp16', False, """Train the model using fp16.""")
-
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train', """Directory where to write event logs and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 10, """Number of batches to run.""")
-tf.app.flags.DEFINE_boolean('log_device_placement', False, """Whether to log device placement.""")
-tf.app.flags.DEFINE_integer('log_frequency', 1000, """How often to log results to the console.""")
+    tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train', """Directory where to write event logs and checkpoint.""")
+    tf.app.flags.DEFINE_integer('max_steps', 10, """Number of batches to run.""")
+    tf.app.flags.DEFINE_boolean('log_device_placement', False, """Whether to log device placement.""")
+    tf.app.flags.DEFINE_integer('log_frequency', 1000, """How often to log results to the console.""")
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
