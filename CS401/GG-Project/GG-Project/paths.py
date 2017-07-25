14f97cfe0cea3bfe0431cc83ba1448a2517bd70a
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-07-25 15:34:01 +0300

    .

--- CS401/GG-Project/GG-Project/paths.py
+++ CS401/GG-Project/GG-Project/paths.py
@@ -67,7 +67,7 @@ allLogsPath = ''
 HDFSRootFolder = 'hdfs://osldevptst01.host.gittigidiyor.net:8020/user/root/'
 HDFSDataFolder = joinPath(HDFSRootFolder, 'data')
 
-may17Folder = 'hdfs://osldevptst02.host.gittigidiyor.net:8888/user/root/searchlogs/'
+may17Folder = 'hdfs://osldevptst02.host.gittigidiyor.net:8020/user/root/searchlogs/'
 textTrainDataFolder = '/soe/cicekm/Projects/offlineData'
 
 if COMPUTERNAME == 'MSI': 
