201713862328346390c4cf3d4ae9a63690ac96f5
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-14 00:06:11 +0300

    .

--- CS401/GG-Project/GG-Project/MainSrc/SparkerMethods.py
+++ CS401/GG-Project/GG-Project/MainSrc/SparkerMethods.py
@@ -40,12 +40,16 @@ def runSparkLogOperatorTests(logs):
     readTextFileTest(logs)
     #extractAllTCJourneysTest()
     
+l = []
 def addPyFiles(sc, dr = joinPath(joinPath(joinPath(gitDir, 'CS401'), 'GG-Project'), 'GG-Project')):
     for filename in os.listdir(dr):
+        if filename in l: continue
+        l.append(filename)
         p = joinPath(dr, filename)
         if filename[-3:] == '.py':
             sc.addPyFile(p) 
         elif os.path.isdir(p):
+            sc.addPyFile(p) 
             sc = addPyFiles(sc, p)
     return sc
 
