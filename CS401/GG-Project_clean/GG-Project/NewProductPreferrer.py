4c5a3c9bd16da490b9f75008e7f643041f6f89e1
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-15 20:15:28 +0300

    .

--- CS401/GG-Project_clean/GG-Project/NewProductPreferrer.py
+++ CS401/GG-Project_clean/GG-Project/NewProductPreferrer.py
@@ -8,14 +8,6 @@ KEY_PREFERRED_ID_LIST ='preferred_ids'
 KEY_PRODUCT_COEFFICIENT = 1
 KEY_CART_COEFFICIENT = 10
 KEY_PAYMENT_COEFFICIENT = 50
-
-def specificPreviousSearches(productLog, search):
-    if search[KEY_TIMESTAMP] < productLog[KEY_TIMESTAMP]:
-        for id in productLog[KEY_FOUR_IDS]:
-            if id in search[KEY_FOUR_IDS]:
-                return True
-    return False
-
 def specificPreviousSearchesWithId(productLog, search):
     onSearch = False
     if isinstance(productLog[KEY_ID], int):
@@ -30,64 +22,29 @@ def specificPreviousSearchesWithId(productLog, search):
                     break
     return onSearch
 
-def findViewedProductIstancesOnSearches(productLog, searches):
-    searches = searches.filter(lambda search: specificPreviousSearches(productLog, search)) \
-                    .filter(lambda search: specificPreviousSearchesWithId(productLog, search))
-    instances = []
-    searches = searches.sortBy(lambda search: search[KEY_TIMESTAMP], ascending = False)
-    if searches.count() > 0:
-        search = searches.first()
-        for i, id in enumerate(search[KEY_ID_LIST]):
-            if isinstance(productLog[KEY_ID], int):
-                if productLog[KEY_ID] == id:
-                    if i > 0:
-                        productLog[KEY_PREFERRED_ID_LIST] = search[KEY_ID_LIST][:i]
-                        instances.append(productLog)
-                        break
-            elif isinstance(productLog[KEY_ID], str):
-                if '%7C' in productLog[KEY_ID]:
-                    processedIds = [int(i) for i in productLog[KEY_ID].split('%7C')]
-                    for pid in processedIds:
-                        for i, id in enumerate(search[KEY_ID_LIST]):  
-                            if pid == id:
-                                if i > 0:
-                                    productLog[KEY_ID] = pid
-                                    productLog[KEY_PREFERRED_ID_LIST] = search[KEY_ID_LIST][:i]
-                                    instances.append(productLog)
-                                    break
-    return instances
+def instanceListFromActions(sp):
+    if sp[1][1][KEY_MODULE] == KEY_MODULE_CART:
+        coefficient = KEY_CART_COEFFICIENT
+    if sp[1][1][KEY_MODULE] == KEY_MODULE_PAYMENT:
+        coefficient = KEY_PAYMENT_COEFFICIENT
+    else:
+        coefficient = KEY_PRODUCT_COEFFICIENT
+    if isinstance(sp[1][1][KEY_ID], int):
+        return coefficient * [(sp[1][1][KEY_ID], sp[0][KEY_ID_LIST][:sp[0][KEY_ID_LIST].index(sp[1][1][KEY_ID])])]
+    else:
+        processedIds = [int(i) for i in productLog[KEY_ID].split('%7C')]
+        s = []
+        for pid in processedIds:
+            if pid in sp[0][KEY_ID_LIST]:
+                s.extend(coefficient * [(sp[1][KEY_ID], sp[0][KEY_ID_LIST][:sp[0][KEY_ID_LIST].index(pid)])])
+        return s
 
-def idsMatched(productLog, viewIstance):
-    if isinstance(productLog[KEY_ID], int):
-        return viewIstance[KEY_ID] == productLog[KEY_ID]
-    elif isinstance(productLog[KEY_ID], str):
-        return str(viewIstance[KEY_ID]) in productLog[KEY_ID]
-
-def specificPreviousViews(productLog, viewIstance):
-    if idsMatched(productLog, viewIstance) and viewIstance[KEY_TIMESTAMP] < productLog[KEY_TIMESTAMP]:
-            for id in productLog[KEY_FOUR_IDS]:
-                if id in viewIstance[KEY_FOUR_IDS]:
-                    return True
-    return False
-
-def findcartedOrPaidProductIstancesOnViews(productLog, viewedProductIstances):
-    viewedIstances = [instance for instance in viewedProductIstances if specificPreviousViews(productLog, instance)]
-    if len(viewedIstances) > 0:
-        if productLog[KEY_MODULE] == KEY_MODULE_CART:
-            coefficient = KEY_CART_COEFFICIENT
-        if productLog[KEY_MODULE] == KEY_MODULE_PAYMENT:
-            coefficient = KEY_PAYMENT_COEFFICIENT
-        else:
-            coefficient = KEY_PRODUCT_COEFFICIENT
-            print_('WARNING: Unexpected productLog on cartedOrPaidProductLogs:\n' + str(productLog))
-        return coefficient * [sorted(viewedIstances, key = lambda instance: -instance[KEY_TIMESTAMP])[0]]
-    return []
- 
-def extendLists(l):
-    nl = []
-    for e in l:
-        nl.extend(e)
-    return nl
+def labelPairs(s):
+    s = []
+    for i in s[1]:
+        s.append(((s[0], i), 1))
+        s.append(((i, s[0]), 0))
+    return s
 
 def getLabeledPairs(searches, productLogs):
     import SparkLogFileHandler
@@ -109,61 +66,11 @@ def getLabeledPairs(searches, productLogs):
         .groupBy(lambda sp: sp[1][1][KEY_TIMESTAMP])\
         .map(lambda tsp: list(tsp[1])[0])
     print_(searchedLogs.count(), "logs", nowStr())
-    def idPairs(sp):
-        if sp[1][1][KEY_MODULE] == KEY_MODULE_CART:
-            coefficient = KEY_CART_COEFFICIENT
-        if sp[1][1][KEY_MODULE] == KEY_MODULE_PAYMENT:
-            coefficient = KEY_PAYMENT_COEFFICIENT
-        else:
-            coefficient = KEY_PRODUCT_COEFFICIENT
-        if isinstance(sp[1][1][KEY_ID], int):
-            return coefficient * [(sp[1][1][KEY_ID], sp[0][KEY_ID_LIST][:sp[0][KEY_ID_LIST].index(sp[1][1][KEY_ID])])]
-        else:
-            processedIds = [int(i) for i in productLog[KEY_ID].split('%7C')]
-            s = []
-            for pid in processedIds:
-                if pid in sp[0][KEY_ID_LIST]:
-                   s.extend(coefficient * [(sp[1][KEY_ID], sp[0][KEY_ID_LIST][:sp[0][KEY_ID_LIST].index(pid)])])
-            return s
-    def pairn(s):
-        s = []
-        for i in s[1]:
-            s.append(((s[0], i), 1))
-            s.append(((i, s[0]), 0))
-        return s
-    pairs = searchedLogs.map(idPairs).flatMap(lambda s: s).flatMap(pairn)
+    pairs = searchedLogs.map().flatMap(lambda s: instanceListFromActions(s))
+    print_(pairs.count(), "pair")
+    pairs = pairs.flatMap(labelPairs)
     print_(pairs.count(), "pair")
     return pairs
-
-def productInstances(logs):
-    import PythonVersionHandler
-    (searches, viewedProductLogs, cartedOrPaidProductLogs) = logs
-    searches = searches.map(idSetter)
-    viewedProductLogs = viewedProductLogs.map(lambda kv: idSetter(kv[1])).collect()
-    viewedProductIstances = extendLists([findViewedProductIstancesOnSearches(productLog, searches) for productLog in viewedProductLogs])
-    PythonVersionHandler.print_high_logging(len(viewedProductIstances), 'product instances have been found from', 
-                       len(viewedProductLogs), 'viewed productLogs on searches by', PythonVersionHandler.nowStr())
-    #viewedProductIstances = sc_().parallelize(viewedProductIstances)
-    cartedOrPaidIstances = cartedOrPaidProductLogs.map(lambda kv: idSetter(kv[1][1])) \
-        .map(lambda productLog: findcartedOrPaidProductIstancesOnViews(productLog, viewedProductIstances))
-    if cartedOrPaidIstances.count() > 0:
-        cartedOrPaidIstances = cartedOrPaidIstances.reduce(lambda a, b: a+b)
-        PythonVersionHandler.print_high_logging(len(cartedOrPaidIstances), 'carted or paid instances have been found from', 
-                           cartedOrPaidProductLogs.count(), 'carted or paid productLogs on searches by', PythonVersionHandler.nowStr())
-    else:
-        cartedOrPaidIstances = []
-        PythonVersionHandler.print_high_logging(len(cartedOrPaidIstances), 'carted or paid instances have been found from', 
-                           cartedOrPaidProductLogs.count(), 'carted or paid productLogs on searches by', PythonVersionHandler.nowStr())
-
-    #return viewedProductIstances.union(sc_().parallelize(cartedOrPaidIstances))
-    return sc_().parallelize(viewedProductIstances + cartedOrPaidIstances)
-
-def pairsList(productLog):
-    pList = []
-    for otherId in productLog[KEY_PREFERRED_ID_LIST]:
-        pList.append((productLog[KEY_ID], otherId))
-        pList.append((otherId, productLog[KEY_ID]))
-    return pList
 
 def trainingInstancesForSingleKeyword(logs):
     import PythonVersionHandler
