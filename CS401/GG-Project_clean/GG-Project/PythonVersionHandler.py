19d16f3ef69253b3091cd371f953758eda4a700f
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-16 02:09:24 +0300

    .

--- CS401/GG-Project_clean/GG-Project/PythonVersionHandler.py
+++ CS401/GG-Project_clean/GG-Project/PythonVersionHandler.py
@@ -20,7 +20,7 @@ def print_(*args):
     global WRITE_OUTPUTS
     if WRITE_OUTPUTS:        
         if paths.COMPUTERNAME == 'osldevptst02':
-            outputFileName = 'output12.txt'
+            outputFileName = 'output13.txt'
         else:
             outputFileName = 'output_local.txt'
         if os.path.isfile(outputFileName):
