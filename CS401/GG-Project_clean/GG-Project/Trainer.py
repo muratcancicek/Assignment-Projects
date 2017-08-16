219360f5f30432d871c6f41168c8b169bb348ccd
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-16 14:08:36 +0300

    .

--- CS401/GG-Project_clean/GG-Project/Trainer.py
+++ CS401/GG-Project_clean/GG-Project/Trainer.py
@@ -88,7 +88,7 @@ def trainPairWiseData(data, dataName = 'Data', modelName = 'Model', evaluate = T
 
 def saveSpecificProduct(products, outputPath):
     import paths, SparkLogFileHandler
-    products = products.map(lambda x: tuple([x[0]]+x[1].toArray()))
+    #products = products.map(lambda x: tuple([x[0]]+x[1].toArray()))
     try: 
        SparkLogFileHandler.saveRDDToHDFS(products, outputPath)
     except:
