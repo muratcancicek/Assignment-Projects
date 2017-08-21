74d8e3781a602e97a37ba12d9b06c306ebdcf5c0
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-21 15:18:31 +0300

    .

--- CS401/GG-Project_clean/GG-Project/StepStepRanking.py
+++ CS401/GG-Project_clean/GG-Project/StepStepRanking.py
@@ -1,5 +1,5 @@
 
-def findProductById(i, path):
+def findProducts_ById(i, path):
     import Trainer, SparkLogFileHandler
     if isinstance(i, list):
         ids = SparkLogFileHandler.sc_().parallelize([int(j) for j in i])
@@ -11,5 +11,31 @@ def findProductById(i, path):
         products = Trainer.getProducts(ids, paths.newProductVectorFolder)
         if products.isEmpty():
             import PythonVersionHandler
-            PythonVersionHandler.print_('Products cannot be found in the database')
+            PythonVersionHandler.print_('Products cannot be found in the database.')
     return products
+
+
+def findProductById(i, path):
+    products = findProducts_ById(i, path)
+    if not products.isEmpty():
+        return products.collect()[0]
+    
+def findPairById(i1, i2, path):
+    import Trainer
+    pairs = Trainer.readLabeledPairs(path)
+    pairs = pairs.filter(lambda p: (i1, i2) == p[0])
+    import PythonVersionHandler
+    PythonVersionHandler.print_(pairs.count(), 'Instances have been found for this pair.')
+    pairsP = pairs.filter(lambda p: 1 == p[1])
+    PythonVersionHandler.print_(pairsP.count(), 'Positive instances have been found for this pair.')
+    pairsN = pairs.filter(lambda p: 0 == p[1])
+    PythonVersionHandler.print_(pairsN.count(), 'Negative instances have been found for this pair.')
+    
+def mmmm(i, path):
+    import Trainer
+    pairs = Trainer.readLabeledPairs(path)
+    data1 = pairs.map(lambda p: p[0][0])
+    data1 = Trainer.getProducts(data1, path)
+    data2 = pairs.map(lambda p: p[0][1])
+    data2 = Trainer.getProducts(data2, path)
+    label = pairs.map(lambda p: p[1])
