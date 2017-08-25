9f83c6c3355357d70e4ce0eaed428ea583349fff
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-25 16:57:30 +0300

    .

--- CS401/GG-Project_clean/GG-Project/NewExtractorRunner.py
+++ CS401/GG-Project_clean/GG-Project/NewExtractorRunner.py
@@ -117,7 +117,7 @@ def bes():
     keyword_name = keyword
     outputFolder = paths.joinPath(paths.HDFSRootFolder, 'weekAugust')
     outputPath = paths.joinPath(outputFolder, keyword_name + '/' + keyword_name + '_extractedLogs')
-    logs = getPreparedLogsFromHDFS(outputPath, filtering = True)
+    logs = FinalizedRunners.getPreparedLogsFromHDFS(outputPath, filtering = True)
     searchNProductLogs = SearchExtractor.searchNProductLogsForSingleKeyword(logs, keyword)
     pairs = NewProductPreferrer.trainingInstancesForSingleKeyword(searchNProductLogs)
     if pairs.isEmpty():
