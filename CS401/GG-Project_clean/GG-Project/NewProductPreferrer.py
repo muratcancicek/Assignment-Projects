ea3a6c63d44f568c40f7d4d6731b024e75f8b7e1
Muratcan Çiçek <muratcancicek0@gmail.com>
2017-08-28 02:46:15 +0300

    .

--- CS401/GG-Project_clean/GG-Project/NewProductPreferrer.py
+++ CS401/GG-Project_clean/GG-Project/NewProductPreferrer.py
@@ -66,8 +66,8 @@ def getLabeledPairsOnSinglePage(searchesNProducts):
     import LumberjackConstants as L
     searchesNProducts = searchesNProducts.filter(lambda sp: isProductIdOnSearch(sp[1][1], sp[0]))
     searchesNProducts = searchesNProducts.map(lambda sp: (sp[1][1][L.KEY_TIMESTAMP], sp))
-    print(searchesNProducts.first())
     searchesNProducts = searchesNProducts.reduceByKey(lambda x1, x2: max(x1, x2, key=lambda x: x[0][L.KEY_TIMESTAMP]))
+    print(searchesNProducts.first())
     pairs = searchesNProducts.flatMap(instanceListFromActions)
     pairs = pairs.flatMap(labelPairs)
     return pairs, searchesNProducts
