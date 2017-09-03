f8b2763450b6180978e02cb040109b1bb49c072b
muratcancicek <akrap16@gmail.com>
2017-09-03 03:21:22 +0300

    .

--- CS401/GG-Project_clean/GG-Project/RunnerOnJupyter.py
+++ CS401/GG-Project_clean/GG-Project/RunnerOnJupyter.py
@@ -4,14 +4,14 @@ def printSeparater():
     for n in range(3):
         PythonVersionHandler.print_('#' * 88)
         
-def runOnJupyter(method = None):
+def runOnJupyter(method = None, setMaster = False):
     printSeparater()
     import PythonVersionHandler, paths, SparkLogFileHandler
     PythonVersionHandler.print_('%s:' % PythonVersionHandler.nowStr(), 'Running on', paths.COMPUTERNAME)
 
     if SparkLogFileHandler.sc_() == None:
         import SparkerMethods
-        SparkerMethods.runSpark(setMaster = False)
+        SparkerMethods.runSpark(setMaster = setMaster)
 
     if method == None:
         import NewExtractorRunner
